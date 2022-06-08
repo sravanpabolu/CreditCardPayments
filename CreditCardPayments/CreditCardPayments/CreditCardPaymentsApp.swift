24ec40e52e69e36441d0503be30dbf7adfc8cd8d
Sravan <sravanpabolu@gmail.com>
2022-06-08 17:14:40 +0530

    initial commit

---
+++ CreditCardPayments/CreditCardPayments/CreditCardPaymentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreditCardPaymentsApp.swift
+//  CreditCardPayments
+//
+//  Created by Sravan Kumar Pabolu on 08/06/22.
+//
+
+import SwiftUI
+
+@main
+struct CreditCardPaymentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
