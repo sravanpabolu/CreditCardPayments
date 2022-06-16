1dd93c21ed3a82b4c095a6803d19c2998ed21d21
Sravan <sravanpabolu@gmail.com>
2022-06-17 00:21:42 +0530

    Constants

---
+++ CreditCardPayments/CreditCardPayments/Misc/Constants.swift
@@ -0,0 +1,24 @@
+//
+//  Constants.swift
+//  CreditCardPayments
+//
+//  Created by Sravan Kumar Pabolu on 16/06/22.
+//
+
+import Foundation
+
+// First Name, Last Name, DOB, Gender, Card details, SSN (Masked)
+
+struct Constants {
+    static let login = "Login"
+    static let register = "Register"
+    
+    static let first_name = "First Name"
+    static let last_name = "Last Name"
+    static let dob = "Date of birth"
+    static let gender = "Gender"
+    static let card_details = "Card Details"
+    static let ssn = "SSN"
+    
+    static let btn_submit = "Submit"
+}
