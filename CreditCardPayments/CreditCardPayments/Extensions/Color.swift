20231b335dc6b9cac4e15ed29042069ec664b78c
Sravan <sravanpabolu@gmail.com>
2022-06-17 00:25:24 +0530

    removed unwanted code

--- CreditCardPayments/CreditCardPayments/Extensions/Color.swift
+++ CreditCardPayments/CreditCardPayments/Extensions/Color.swift
@@ -7,45 +7,6 @@
 
 import SwiftUI
 
-//extension UIColor {
-//    convenience init(hex: Int) {
-//        let components = (
-//            R: CGFloat((hex >> 16) & 0xff) / 255,
-//            G: CGFloat((hex >> 08) & 0xff) / 255,
-//            B: CGFloat((hex >> 00) & 0xff) / 255
-//        )
-//        self.init(red: components.R, green: components.G, blue: components.B, alpha: 1)
-//    }
-//}
-//
-//extension Color {
-//    public init(hex: Int) {
-//        self.init(UIColor(hex: hex))
-//    }
-//
-//    static func hexColour(hexValue:UInt32) -> Color {
-//        let red = Double((hexValue & 0xFF0000) >> 16) / 255.0
-//        let green = Double((hexValue & 0xFF00) >> 8) / 255.0
-//        let blue = Double(hexValue & 0xFF) / 255.0
-//        return Color(red:red, green:green, blue:blue)
-//    }
-//}
-
-//extension Color {
-//    static var registerScreenBGColor: Color {
-////        Color.hexColour(hexValue: 0xC7B7E0)
-//        Color(hex: 0xC7B7E0)
-//    }
-//
-//    static var textFieldTitleColor: Color {
-//        Color.hexColour(hexValue: 0x7A55B7)
-//    }
-//
-//    static var buttonBGColor: Color {
-//        Color.hexColour(hexValue: 0x8764C1)
-//    }
-//}
-
 extension Color {
     static var titleColor: Color {
         Color(red: 135/255, green: 103/255, blue: 192/255)
