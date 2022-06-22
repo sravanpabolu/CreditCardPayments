f0cb382bf5cd42084f389afca3c53b72ac5cacdc
Sravan <sravanpabolu@gmail.com>
2022-06-22 13:57:18 +0530

    Reusable - Enums and Local client

---
+++ CreditCardPayments/CreditCardPayments/Reusable/Enums.swift
@@ -0,0 +1,37 @@
+//
+//  Enums.swift
+//  CreditCardPayments
+//
+//  Created by Sravan Kumar Pabolu on 22/06/22.
+//
+
+import Foundation
+
+enum EndPoint {
+    case recentTransactions
+    
+    func getUrl() -> URL? {
+        switch self {
+        case .recentTransactions:
+            return Bundle.main.url(forResource: "RecentTransactions", withExtension: "json")
+        }
+    }
+}
+
+enum DataFetchError: Error {
+    case url, response, parsing
+    case custom(description: String)
+    
+    func errorDescription() -> String {
+        switch self {
+        case .url:
+            return "URL Error"
+        case .response:
+            return "Response Error"
+        case .parsing:
+            return "Parsing Error"
+        case .custom(description: let description):
+            return description
+        }
+    }
+}
---
+++ CreditCardPayments/CreditCardPayments/Reusable/LocalClient.swift
@@ -0,0 +1,36 @@
+//
+//  LocalClient.swift
+//  CreditCardPayments
+//
+//  Created by Sravan Kumar Pabolu on 22/06/22.
+//
+
+import Foundation
+import Combine
+
+protocol DataFetchService {
+    func getData<T: Codable>(for endpoint: EndPoint, type: T.Type) throws -> AnyPublisher<T, Error>
+}
+
+struct LocalClient: DataFetchService {
+    func getData<T>(for endpoint: EndPoint, type: T.Type) throws -> AnyPublisher<T, Error> where T : Decodable, T : Encodable {
+     
+        print("Fetching from local json")
+        
+        guard let url = endpoint.getUrl() else {
+            throw DataFetchError.url
+        }
+        
+        let aPublisher = Future<T, Error> { promise in
+            do {
+                let data = try Data(contentsOf: url)
+                let parsedData = try JSONDecoder().decode(T.self, from: data)
+                return promise(.success(parsedData))
+            } catch {
+                return promise(.failure(DataFetchError.parsing))
+            }
+        }
+        
+        return aPublisher.eraseToAnyPublisher()
+    }
+}
