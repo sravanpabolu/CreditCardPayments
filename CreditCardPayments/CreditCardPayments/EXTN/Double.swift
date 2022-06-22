edf182a0f6f04385b6e9ddc458e7caec4aad1b6c
Sravan <sravanpabolu@gmail.com>
2022-06-22 20:16:05 +0530

    Extensions

---
+++ CreditCardPayments/CreditCardPayments/EXTN/Double.swift
@@ -0,0 +1,19 @@
+//
+//  Double.swift
+//  CreditCardPayments
+//
+//  Created by Sravan Kumar Pabolu on 22/06/22.
+//
+
+import Foundation
+
+extension Double {
+    func round(to decimalPlaces: Int) -> Double {
+        let precisionNumber = pow(10,Double(decimalPlaces))
+        var n = self // self is a current value of the Double that you will round
+        n = n * precisionNumber
+        n.round()
+        n = n / precisionNumber
+        return n
+    }
+}
