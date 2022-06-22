9608aa490a579e565fbf5d36d403517caf35a0d8
Sravan <sravanpabolu@gmail.com>
2022-06-22 13:27:32 +0530

    User details are retrieved and MPIN is verified.

--- CreditCardPayments/CreditCardPayments/ViewModels/LoginViewModel.swift
+++ CreditCardPayments/CreditCardPayments/ViewModels/LoginViewModel.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+class LoginViewModel: ObservableObject {
+    @Published var user: User?
+
+    func retrieveUser() {
+        if let userData = UserDefaults.standard.data(forKey: Constants.Defaults.loginKey) {
+            let decoder = JSONDecoder()
+            let newUser = try? decoder.decode(User.self, from: userData)
+            user = newUser
+        } else {
+            user = nil
+        }
+    }
+    
+    func verifyLogin(with mPIN: String) -> Bool {
+        mPIN == user?.mpin ? true : false
+    }
+}
