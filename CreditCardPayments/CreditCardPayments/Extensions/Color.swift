1675474ee4cf0a3ee1a8a7e81ef3a7e2fcb760f3
Sravan <sravanpabolu@gmail.com>
2022-06-23 14:44:09 +0530

    Resolved Merge conflicts

--- CreditCardPayments/CreditCardPayments/Extensions/Color.swift
+++ CreditCardPayments/CreditCardPayments/Extensions/Color.swift
@@ -23,3 +23,21 @@ extension Color {
         Color(red: 135/255, green: 103/255, blue: 192/255)
     }
 }
+
+extension Color {
+    static var debitCapsuleBG: Color {
+        Color(red: 255/255, green: 216/255, blue: 229/255)
+    }
+    
+    static var creditCapsuleBG: Color {
+        Color(red: 222/255, green: 253/255, blue: 220/255)
+    }
+    
+    static var goldBG: Color {
+        Color(red: 255/255, green: 215/255, blue: 0/255)
+    }
+    
+    static var goldFont: Color {
+        Color(red: 212/255, green: 175/255, blue: 55/255)
+    }
+}
